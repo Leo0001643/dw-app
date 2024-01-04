7a5cbfc5836b2fdc7e0d1bee777b3375f29f668a
Lance <Lace@gmail.com>
2024-01-04 22:57:06 +0800

    feature:首页倒计时

--- lib/ui/main/home/text_timer/text_timer_logic.dart
+++ lib/ui/main/home/text_timer/text_timer_logic.dart
@@ -16,6 +16,7 @@ class TextTimerLogic {
   StreamSubscription? loginStream;
   var count = 100;
   String? type;
+  Timer? countdownTimer;
 
   TextTimerLogic({required this.type});
 
@@ -33,15 +34,17 @@ class TextTimerLogic {
 
   void loadData(
       GameKindGameKindList gameKind, Pc28LottoEntity pc28lottoEntity) {
+    //测试用
     onReady();
     HttpService.getPc28LottoList().then((value) {
       state.pc28Lotto.value = value;
       state.pc28Lotto.refresh();
       print(">>item.gameType>>>" + gameKind.gameCode.toString());
+      /*   //测试用
       // Timer.periodic(Duration(seconds: 1), (timer) {
       //   count--;
       //   state.text_timer.value = count.toString();
-      // });
+      // });*/
       for (Pc28LottoRooms item in pc28lottoEntity.rooms!) {
         print(">>item.gameType" + item.gameType.toString());
         // 判断 gameCode 和 gameType 是否相等
@@ -49,7 +52,6 @@ class TextTimerLogic {
           // 执行倒计时逻辑
           state.text_timer.value = gameKind.gameCode.toString();
           loadTimerData(item);
-          break; // 找到匹配的 gameType 后可以退出循环
         }
       }
     });
@@ -67,11 +69,21 @@ class TextTimerLogic {
 
   void _calculateCountdown(
       Pc28LottoRooms pc28lottoRoom, Pc28PlanEntity pc28PlanEntity) {
+    countdownTimer?.cancel();
     // 服务器的误差时间
     var diffTime =
         pc28PlanEntity.timestamp! - DateTime.now().millisecondsSinceEpoch;
-    Timer.periodic(Duration(seconds: 1), (timer) {
+    countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      /*  测试
+   count--;
+      state.text_timer.value = count.toString();*/
       timeCountOnly(diffTime, pc28lottoRoom, pc28PlanEntity);
+      // 如果倒计时结束，取消计时器
+      if (count <= 0) {
+        countdownTimer?.cancel();
+        // 你可能还想执行一些其他操作，或者重新启动倒计时
+        // 例如，重新调用 _calculateCountdown 或执行其他逻辑
+      }
     });
   }
 
@@ -82,70 +94,53 @@ class TextTimerLogic {
     Map<String, dynamic> roominf = pc28lottoRoom.toJson();
 
     for (String key in allTime.keys) {
-      if (roominf[key]?.stateMsg != 0) {
-        if (roominf[key]?.stateMsg == 1) {
-          roomcountdown[key + 'Time'] = "维护中";
-        } else if (roominf[key]?.stateMsg == 3) {
-          roomcountdown[key + 'Time'] = "已关盘";
-        } else if (roominf[key]?.stateMsg == 4) {
-          roomcountdown[key + 'Time'] = "已休市";
-        }
-        roomcountdown[key + 'Term'] = '--';
-        roomcountdown[key + 'Notice'] = allTime[key]['msg'] ?? '';
-        continue;
-      }
+      if (allTime[key]['data'].length > 1) {
+        for (int s = 0; s < allTime[key]['data'].length - 1; s++) {
+          int onlineT = DateTime.now().millisecondsSinceEpoch +
+              int.parse(diffTime.toString());
+          if (onlineT <= allTime[key]['data'][s + 1]['openTime']) {
+            int openT =
+                (int.parse(allTime[key]['data'][s + 1]['openTime'].toString()) -
+                        onlineT) ~/
+                    1000;
+            roomcountdown[key + 'OpenResult'] = openT;
+            if (openT == 0) {
+              roomcountdown[key + 'OpenResult'] = '开奖中';
+            }
 
-      if (allTime[key]['code'] == 100020) {
-        roomcountdown[key + 'Time'] = "Index.等待开盘";
-        roomcountdown[key + 'Term'] = '--';
-        roomcountdown[key + 'Notice'] = allTime[key]['msg'];
-      } else {
-        if (allTime[key]['data'].length > 1) {
-          for (int s = 0; s < allTime[key]['data'].length - 1; s++) {
-            int onlineT =
-                DateTime.now().millisecondsSinceEpoch + int.parse(diffTime);
-            if (onlineT <= allTime[key]['data'][s + 1]['openTime']) {
-              int openT = (int.parse(allTime[key]['data'][s + 1]['openTime']) -
-                      onlineT) ~/
-                  1000;
-              roomcountdown[key + 'OpenResult'] = openT;
-              if (openT == 0) {
-                roomcountdown[key + 'OpenResult'] = 'MiPai.开奖中';
-              }
-
-              if (onlineT < allTime[key]['data'][s]['closeTime'] &&
-                  onlineT > allTime[key]['data'][s]['openTime']) {
-                int rrtime = allTime[key]['data'][s]['closeTime'];
-                int showT = (rrtime - onlineT) ~/ 1000;
-                String showtime = secToTime(showT);
-                roomcountdown[key + 'Time'] = showtime;
-                roomcountdown[key + 'Term'] = allTime[key]['data'][s]['term'];
-                break;
-              } else if (onlineT > allTime[key]['data'][s]['closeTime'] &&
-                  onlineT < allTime[key]['data'][s + 1]['openTime']) {
-                roomcountdown[key + 'Time'] = "Index.封盘中";
-                roomcountdown[key + 'Term'] = allTime[key]['data'][s]['term'];
-                break;
-              }
+            if (onlineT < allTime[key]['data'][s]['closeTime'] &&
+                onlineT > allTime[key]['data'][s]['openTime']) {
+              int rrtime = allTime[key]['data'][s]['closeTime'];
+              int showT = (rrtime - onlineT) ~/ 1000;
+              String showtime = secToTime(showT);
+              roomcountdown[key + 'Time'] = showtime;
+              roomcountdown[key + 'Term'] = allTime[key]['data'][s]['term'];
+              break;
+            } else if (onlineT > allTime[key]['data'][s]['closeTime'] &&
+                onlineT < allTime[key]['data'][s + 1]['openTime']) {
+              roomcountdown[key + 'Time'] = "封盘中";
+              roomcountdown[key + 'Term'] = allTime[key]['data'][s]['term'];
+              break;
             }
           }
-        } else if (allTime[key]['data'].length == 1) {
-          int onlineT =
-              DateTime.now().millisecondsSinceEpoch + int.parse(diffTime);
-          if (onlineT < allTime[key]['data'][0]['closeTime'] &&
-              onlineT > allTime[key]['data'][0]['openTime']) {
-            int rrtime = allTime[key]['data'][0]['closeTime'];
-            int showT = (rrtime - onlineT) ~/ 1000;
-            String showtime = secToTime(showT);
-            roomcountdown[key + 'Time'] = showtime;
-            roomcountdown[key + 'Term'] = allTime[key]['data'][0]['term'];
-          } else if (onlineT < allTime[key]['data'][0]['openTime']) {
-            roomcountdown[key + 'Time'] = "Index.封盘中";
-            roomcountdown[key + 'Term'] = allTime[key]['data'][0]['term'];
-          }
+        }
+      } else if (allTime[key]['data'].length == 1) {
+        int onlineT =
+            DateTime.now().millisecondsSinceEpoch + int.parse(diffTime);
+        if (onlineT < allTime[key]['data'][0]['closeTime'] &&
+            onlineT > allTime[key]['data'][0]['openTime']) {
+          int rrtime = allTime[key]['data'][0]['closeTime'];
+          int showT = (rrtime - onlineT) ~/ 1000;
+          String showtime = secToTime(showT);
+          roomcountdown[key + 'Time'] = showtime;
+          roomcountdown[key + 'Term'] = allTime[key]['data'][0]['term'];
+        } else if (onlineT < allTime[key]['data'][0]['openTime']) {
+          roomcountdown[key + 'Time'] = "封盘中";
+          roomcountdown[key + 'Term'] = allTime[key]['data'][0]['term'];
         }
       }
       state.text_timer.value = roomcountdown[key + 'Time'];
+
     }
   }
 
