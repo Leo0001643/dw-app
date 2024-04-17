a8edf0d1a779e2ea105b46c93b6f77129b461ba2
Lance <Lance@gmail.com>
2024-04-17 12:57:31 +0800

    优化版本更新功能，设置新增检查更新

--- android/app/src/main/kotlin/com/soushin/leisure/boya/MainActivity.kt
+++ android/app/src/main/kotlin/com/soushin/leisure/boya/MainActivity.kt
@@ -1,8 +1,20 @@
 package com.soushin.leisure.boya
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.core.content.FileProvider
 import io.flutter.embedding.android.FlutterActivity
+import java.io.File
 
 class MainActivity: FlutterActivity() {
 
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("current channel is boya ")
+
+    }
+
+
+
 }
